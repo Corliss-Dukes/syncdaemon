72e73b1e98117f7713000258b4ed11f2e1f580c9
Kcils360 <dukes.gregoryd@gmail.com>
2020-07-01 09:15:15 -0700

    Initial app creation

---
+++ syncdaemon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace syncdaemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
