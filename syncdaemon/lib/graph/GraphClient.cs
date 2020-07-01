737091c33f008089ea77fbe3e65340df8f0d1d11
Kcils360 <dukes.gregoryd@gmail.com>
2020-07-01 12:17:42 -0700

    more clean up

--- syncdaemon/lib/graph/GraphClient.cs
+++ syncdaemon/lib/graph/GraphClient.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Microsoft.Identity.Client;
 using Microsoft.Graph;
 using Microsoft.Extensions.Configuration;
